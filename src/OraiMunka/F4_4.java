0134094cb0be44fa77cd11d28ea723e443764ad4
Patrik Molnár <lylei.lanar@gmail.com>
2021-06-02 12:09:01 +0200

    Feladatgyűjtemény 4/4 (Zsófi)

---
+++ src/OraiMunka/F4_4.java
@@ -0,0 +1,35 @@
+package OraiMunka;
+
+public class F4_4 {
+
+    /*
+        4 3 2 1
+        4 3 2
+        4 3
+        4
+     */
+
+
+    public static void main(String[] args) {
+        piramisKiirasEcceru();
+        System.out.println();
+        piramisKiirasCiklussal(16,8,14);
+    }
+
+    public static void piramisKiirasEcceru() {
+        System.out.println("4 3 2 1\n4 3 2\n4 3\n4");
+
+    }
+
+    public static void piramisKiirasCiklussal(int meret, int min, int max) {
+
+        for (int i = min; i <= max; i++) {
+
+            for (int j = meret; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
