c95961d03b8b1890c1a8457571278036e6bc8cfc
Patrik Molnár <lylei.lanar@gmail.com>
2021-06-02 09:22:05 +0200

    D4 class módosítása

--- src/D4.java
+++ src/D4.java
@@ -1,17 +1,60 @@
+import java.awt.image.renderable.ContextualRenderedImageFactory;
+import java.io.FileOutputStream;
+import java.util.Locale;
+import java.util.Random;
+
 public class D4 {
+
+    static public final String TANAR = "PATRIK";
+
+    enum TANULO {ZSOFI, MAGDI, VILI, JULI}
+
     public static void main(String[] args) {
 
-        int vszam = (int) ((Math.random() * 4) + 1);
+        for (int i = 0; i < 20; i++) {
 
-        if (vszam == 1) {
-            System.out.println("Magdi");
-        } else if (vszam == 2) {
-            System.out.println("Juli");
-        } else if (vszam == 3) {
-            System.out.println("Zsófi");
-        } else if (vszam == 4) {
-            System.out.println("Vili");
-        } else
-            System.out.println("Patrik");
+            System.out.println(sorsol(true));
+        }
+
+    }
+
+    private static String formazNevet(String nev) {
+        nev = nev.toLowerCase();
+        return nev.substring(0, 1).toUpperCase() + nev.substring(1, nev.length());
+    }
+
+    public static String sorsol(boolean tanarral) {
+
+        if (tanarral) {
+            int kiRnd = (int) (Math.random() * (TANULO.values().length + 1));
+
+            if (kiRnd == TANULO.values().length) {
+                return formazNevet(TANAR);
+            }
+        }
+        return formazNevet(TANULO.values()[(int) (Math.random() * TANULO.values().length)].toString());
+    }
+
+    public static String[] sorsolSorrend(boolean tanarral) throws Exception {
+
+        String[] emberek = new String[TANULO.values().length];
+
+        String rndEmber = sorsol(tanarral);
+        emberek[0] = rndEmber;
+
+
+//        for (int i = 1; i < TANULO.values().length; i++) {
+//
+//            while (vanBenneIlyenEmber()) {
+//
+//
+//
+//            }
+//        }
+        throw new Exception("Nincs kész!");
+    }
+
+    public static boolean vanBenneIlyenEmber() throws Exception {
+        throw new Exception("Nincs kész!");
     }
 }
