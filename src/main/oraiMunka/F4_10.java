03639f97a7a95f11c63a133a37694009c5497abc
Patrik Molnár <lylei.lanar@gmail.com>
2021-06-04 11:57:49 +0200

    Feladatgyűjtemény 4/10-13 Háromszög (Közös)

--- src/main/oraiMunka/F4_10.java
+++ src/main/oraiMunka/F4_10.java
@@ -6,7 +6,16 @@ public class F4_10 {
 
     public static void main(String[] args) {
 
-        kiirSzerkeszthetoseg(szerkeszthetoE(bekerHaroszogOldalai()));
+        int[] haromszog = bekerHaroszogOldalai();
+        boolean szerkesztheto = szerkeszthetoE(haromszog);
+
+        kiirSzerkeszthetoseg(szerkesztheto);
+        if (szerkesztheto) {
+
+            kiirHaromszogTipus(haromszog);
+            kiirKerulet(haromszog);
+            kiirTerulet(haromszog);
+        }
 
     }
 
@@ -15,7 +24,7 @@ public class F4_10 {
         int[] haromszog = new int[3];
 
         for (int i = 0; i < haromszog.length; i++) {
-            System.out.println("Kérem adja meg a háromszög (" + (char) (i + 97) + ") oldalát");
+            System.out.print("Kérem adja meg a háromszög (" + (char) (i + 97) + ") oldalát: ");
             haromszog[i] = beker();
         }
 
@@ -43,14 +52,60 @@ public class F4_10 {
         return false;
     }
 
-
     public static void kiirSzerkeszthetoseg(boolean szerkesztheto) {
         if (szerkesztheto) {
-            System.out.println(" A megadott háromszög szerkeszthető");
+            System.out.println("A megadott háromszög szerkeszthető");
 
         } else {
-            System.out.println(" A megadott háromszög NEM szerkeszthető.");
+            System.out.println("A megadott háromszög NEM szerkeszthető.");
         }
+    }
+
+    public static void kiirHaromszogTipus(int[] haromszog) {
+        if ((haromszog[0] == haromszog[1]) && (haromszog[1] == haromszog[2])) {
+            System.out.println("Egyenlő oldalú háromszög!");
+        } else if ((haromszog[0] == haromszog[1]) || (haromszog[1] == haromszog[2]) || (haromszog[0] == haromszog[2])) {
+            System.out.println("Egyenlő szárú háromszög!");
+        } else {
+            System.out.println("Általános háromszög!");
+        }
+    }
+
+  //kerület metódusok
+    public static void kiirKerulet(int[] haromszog) {
+
+        System.out.println("A háromszög kerulete: " + szamolKerulet(haromszog));
+    }
+
+    public static int szamolKerulet(int[] haromszog) {
+        // K = a + b + c => kerulet = haromszog[0]+haromszog[1]+haromszog[2]
+
+        int kerulet = 0;
+
+        for (int i = 0; i < haromszog.length; i++) {
+
+            kerulet = kerulet + haromszog[i];
+
+        }
+
+        return kerulet;
+    }
+
+  //terület metódusok
+    public static void kiirTerulet(int[] haromszog) {
+
+
+        System.out.println("A háromszög területe :  " + szamolTerulet(haromszog));
+
+    }
+
+    public static double szamolTerulet(int[] haromszog) {
+
+        // s =(amb+c)/2 --- kerület fele
+
+        double s = szamolKerulet(haromszog) / 2;
+        return  Math.sqrt(s * (s - haromszog[0]) * (s - haromszog[1]) * (s - haromszog[2]));
+
     }
 
 
