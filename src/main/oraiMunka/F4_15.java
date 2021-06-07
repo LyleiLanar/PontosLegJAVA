84a98693d4bc33071ff1f0c5a09f1d747931ae70
Patrik Molnár <lylei.lanar@gmail.com>
2021-06-07 19:55:00 +0200

    Feladatgyűjtemény 4/14-17

---
+++ src/main/oraiMunka/F4_15.java
@@ -0,0 +1,37 @@
+package main.oraiMunka;
+
+import java.util.Scanner;
+
+public class F4_15 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy mondatot!");
+        String mondat = sc.nextLine();
+
+        for (int i = 0; i < mondat.length(); i++) {
+            if (mondat.charAt(i) != ' ') {
+                System.out.print(mondat.charAt(i));
+            }
+        }
+        System.out.println();
+
+        System.out.println(mondat.replaceAll("k", "@"));
+
+        kiirMassal(mondat,'k', '@');
+
+    }
+
+    public static void kiirMassal(String mondat, char mit, char mire) {
+
+        for (int i = 0; i < mondat.length(); i++) {
+
+            if (mondat.charAt(i) == mit) {
+                System.out.print(mire);
+            } else {
+                System.out.print(mondat.charAt(i));
+            }
+        }
+        System.out.println();
+    }
+}
