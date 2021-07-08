ae243608abbbca3b80cdf55044134fdbaeb638a4
Molnár Patrik <lylei.lanar@gmail.com>
2021-07-08 09:44:20 +0200

    Patrik voltam

--- src/main/penya/Penya.java
+++ src/main/penya/Penya.java
@@ -2,6 +2,6 @@ package main.penya;
 
 public class Penya {
     public static void main(String[] args) {
-        System.out.println("Penya voltam");
+        System.out.println("Patrik voltam");
     }
 }
